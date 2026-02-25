56c1d249cd694b00b41d06f8eb4ac7af56e41bb5
JGP-1208 <josgp1208@gmail.com>
2026-02-25 16:40:26 -0600

    Update ej3.c

--- soluciones/ej3.c
+++ soluciones/ej3.c
@@ -1,6 +1,18 @@
-
 #include <stdio.h>
+
 int main(){
-    // Resolver ejercicio 3
+    int a[3];
+    int b[3];
+    int c[3];
+
+    for(int i=0; i<3; i++){
+        printf("&a[%p]\n",&a[i]);
+    }
+    for(int j=0; j<3; j++){
+        printf("&b[%p]\n",&b[j]);
+    }
+    for(int k=0; k<3; k++){
+        printf("&c[%p]\n",&c[k]);
+    }
     return 0;
 }
