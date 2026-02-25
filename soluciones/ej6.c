2823d7cf90065c7ebe1252abe96e4f7c59e7bbe9
JGP-1208 <josgp1208@gmail.com>
2026-02-25 16:42:27 -0600

    Update ej6.c

--- soluciones/ej6.c
+++ soluciones/ej6.c
@@ -1,6 +1,12 @@
-
 #include <stdio.h>
+
 int main(){
-    // Resolver ejercicio 6
+    int m[2][4];
+    
+    for(int i=0; i<2; i++){
+        for(int j=0; j<4; j++){
+            printf("&m[%p][%p]\n",&m[i][j]);
+        }
+    }
     return 0;
 }
