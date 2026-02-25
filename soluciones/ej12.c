2b910a0bc91531f63909e091818b89477882ce2a
JGP-1208 <josgp1208@gmail.com>
2026-02-25 16:36:51 -0600

    Update ej12.c

--- soluciones/ej12.c
+++ soluciones/ej12.c
@@ -1,6 +1,14 @@
+//* Se sabe:
+int m[3][5];
+base = 4000
 
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 12
-    return 0;
-}
+Calcula:
+1. dirección de m[2][4]
+R= 4000+(2×5+4)×4    —>    4000+14×4=4056
+
+2. dirección de m[1][3]
+R= 4000+(1×5+3)×4    —>    4000+8×4=4032
+
+3. diferencia entre ambas
+R= 4056-4032 = 24 bytes
+*//
