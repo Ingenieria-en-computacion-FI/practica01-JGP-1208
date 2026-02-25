9ecb3d5a8c800d044b4ba99458ea36adcddf4466
JGP-1208 <josgp1208@gmail.com>
2026-02-25 16:36:17 -0600

    Update ej11.c

--- soluciones/ej11.c
+++ soluciones/ej11.c
@@ -1,6 +1,15 @@
+//* Salida:
+&a[0] = 1000
+&a[1] = 1008
+&a[2] = 1016
 
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 11
-    return 0;
-}
+Determina:
+tamaño del tipo
+R= Podemos ver que hay una diferencia de 8 unidades por lo que podemos decir que el tamaño de director es de 8 Bytes
+
+tipo posible
+R= Puede ser de tipo Double
+
+fórmula usada
+R= base + i×8
+*//
